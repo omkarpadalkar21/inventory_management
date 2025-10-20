e73ac060a340d4edc5ce7268d7e5845408073a98
omkar <omkarpadalkar21@gmail.com>
2025-10-20 19:04:08 +0530

    Add postgreSql setup and config

--- src/main/java/com/adv_java/inventory_management/InventoryManagementApplication.java
+++ src/main/java/com/adv_java/inventory_management/InventoryManagementApplication.java
@@ -3,11 +3,17 @@ package com.adv_java.inventory_management;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.TimeZone;
+
 @SpringBootApplication
 public class InventoryManagementApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(InventoryManagementApplication.class, args);
-	}
+    public static void main(String[] args) {
+        TimeZone defaultTz = TimeZone.getTimeZone("Asia/Kolkata");
+        TimeZone.setDefault(defaultTz);
+        System.setProperty("user.timezone", "Asia/Kolkata");
+
+        SpringApplication.run(InventoryManagementApplication.class, args);
+    }
 
 }
